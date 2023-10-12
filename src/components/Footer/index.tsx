2ebbb46d4fc8bbe46125a2023237b131f25709c4
Chris <chrisdietrich3666@gmail.com>
2023-10-11 21:39:39 -0400

    first commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import { Container } from "@mui/material";
+import Typography from "@mui/joy/Typography";
+
+import React from "react";
+
+const index = () => {
+  return (
+    <Container>
+      <Typography level="h2">
+        Sign up to my newsletter for exclusive updates
+      </Typography>
+    </Container>
+  );
+};
+
+export default index;
