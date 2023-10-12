29204a4fcee1de932ce1bbe12c0cd324b02a12ff
Chris <chrisdietrich3666@gmail.com>
2023-10-11 21:50:34 -0400

    styled footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,27 @@
-import { Container } from "@mui/material";
+import { Container, Button } from "@mui/material";
 import Typography from "@mui/joy/Typography";
+import TextField from "@mui/material/TextField";
 
 import React from "react";
 
 const index = () => {
   return (
     <Container>
-      <Typography level="h2">
+      <Typography level="h2" sx={{ textAlign: "center" }}>
         Sign up to my newsletter for exclusive updates
       </Typography>
+      <div>
+        <TextField id="outlined-basic" label="First Name" variant="outlined" />
+        <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+        <TextField id="outlined-basic" label="Email" variant="outlined" />
+        <Button>Sign Up</Button>
+      </div>
+      <div>
+        <Typography sx={{ textAlign: "center" }}>
+          Sign up to my newsletter for exclusive updates
+        </Typography>
+        
+      </div>
     </Container>
   );
 };
