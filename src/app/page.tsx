d29511184040816e6fa91fc9430fc61040e52ff4
Chris <chrisdietrich3666@gmail.com>
2023-10-15 16:47:12 -0400

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 // https://www.janehindmarchart.com/portfolio
-import styles from "./page.module.css";
 import Slideshow from "@/components/Slideshow";
 
+import { Container } from "@mui/material";
 
+import Typography from "@mui/joy/Typography";
 
-
-
+import styles from "./page.module.css";
 
 export default function Home() {
- 
   return (
     <main className={styles.main}>
       <Slideshow />
+      <Container>
+        <Typography sx={{ textAlign: "center", my: 3 }} component="h5">
+          Available Works
+        </Typography>
+      </Container>
     </main>
   );
 }
